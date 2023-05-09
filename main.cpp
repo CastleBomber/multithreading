5453c0d0cb68b9a38d23d343a16a3f0ef1bace9c
cBombs <richie.ucsb@gmail.com>
2023-05-08 18:12:07 -0700

    start with pthread

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/**
+ * Author: CBOMBS
+ * Date: May 8th, 2023
+ *
+ * Shortcuts:
+ *  VS Code:
+ * 	    c++ VS Code clang-formatter: shift+alt+f
+ *
+ *
+ *  Visual Studio:
+ *      code folding: select region, ctrl+m+m
+ *      full-screen: shift+alt+enter
+ *		solution explorer: shift+alt+L
+ *      (start debugger to access watchlist)
+ *      watchlist: ctrl+alt+W,1
+ *      add to watchlist: shift+F9
+ *
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <chrono>
+#include <pthread>
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+}
